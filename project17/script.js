fa3d7c98c26f59a10a8ee74364b85fe2e5a39c1f
Mayur Thokare <mayurthokare90@gmail.com>
2026-07-07 14:38:14 +0530

    Script.js

--- project17/script.js
+++ project17/script.js
@@ -12,3 +12,188 @@
 // console.log(`student ${isstudent}.`);
 
 
+// const num1 = 10
+// const num2 = 52
+
+// console.log("Addition = ", num1+num2);
+// console.log("Subration = ", num1-num2);
+// console.log("Multiplcation = ", num1*num2);
+// console.log("Division = ", num1/num2);
+// console.log("Modulus = ", num1%num2);
+
+
+// let num = "150";
+
+// console.log(Number(num));
+// console.log(Boolean(num));
+// console.log(String(num));
+
+
+// let age = 18;
+
+// if(age<13){
+//     console.log("child");
+// } else if (age >= 13 && <= 17){
+//     console.log("Teenager");
+// }else if (age >= 18 && <= 59){
+//     console.log("Adult");
+// }else{
+//     console.log("Senior Citizen");
+// }
+
+
+
+
+// let num = 825
+
+// if(num % 2 === 0) {
+//     console.log("Even");
+// }else {
+//     console.log("Odd");
+// }
+
+
+
+// let num = 20;
+ 
+// if(num > 0) {
+//     console.log("Positive");
+// }else if (num < 0) {
+//     console.log("Negative");
+// }else{
+//     console.log("Zero");
+// }
+
+
+
+
+// let num1 = 10;
+// let num2 = 20;
+
+// if (num1 > num2){
+//     console.log(num1, " is greater");
+// }else if (num2 > num1){
+//     console.log(num2, " is greater")
+// } else {
+//     console.log("Both numbers are equal");
+// }
+
+
+// let password = "fhcjdnbccj562"
+
+// if (password.length >= 8){
+//     console.log("Srong Password");
+// }else if (password.length){
+//     console.log("Weak Password");
+// }else {
+//     console.log("Enter password");
+// }
+
+
+
+// let marks = 78;
+
+// if(marks >= 90 && marks <= 100){
+//     console.log("A");
+// }else if (marks >= 80 && marks <= 89){
+//     console.log("B");
+// }else if (marks >= 70 && marks <= 79){
+//     console.log("C");
+// }else if (marks >= 60 && marks <= 69){
+//     console.log("D");
+// }else if (marks < 60){
+//     console.log("Fail")
+// }
+
+
+
+// let username = "Admin"
+// let password = "1234@"
+
+// if (username == "Admin" && password == "1234@"){
+//     console.log("Login Successful");
+// }else {
+//     console.log("Invalid username or passwird");
+// }
+
+
+// let single = "red"
+
+// if (single == "red"){
+//     console.log("stop");
+// } else if (single == "yellow"){
+//     console.log("Ready");
+// } else if (single == "green") {
+//     console.assert.log("Go");
+// }
+
+
+// let age = 20;
+// let nationality = "Indian"
+
+// if (age >= 18 && nationality === "Indian") {
+//     console.log("Eligible");
+// } else {
+//     console.log("Not Eligible");
+// }
+
+
+// let bill = 6000;
+// let discount = (bill * 20) /100;
+// let final = bill -discount;
+
+// if(bill>=5000){
+//     let discount = (bill * 20) /100;
+//     let final = bill -discount;
+    
+//     console.log("Bill = ",bill);
+//     console,log("Discount = ", discount);
+//     console,log("Final Bill= ", final);
+// } else if(bill>=3000){
+//     let discount = (bill * 15) /100;
+//     let final = bill -discount;
+    
+//     console.log("Bill = ",bill);
+//     console,log("Discount = ", discount);
+//     console,log("Final Bill= ", final);
+// } else if(bill>=1000){
+//     let discount = (bill * 10) /100;
+//     let final = bill -discount;
+    
+//     console.log("Bill = ",bill);
+//     console,log("Discount = ", discount);
+//     console,log("Final Bill= ", final);
+// } else {
+//     console.log("No Discount");
+// }
+
+
+
+// let Name = "mayur"
+// let Age = 19
+// let marks = 85
+// let Course = "Science"
+
+// if(Age >=  && marks>=70){
+//     switch(Course){
+//         case "Science"
+//             console.log("Admission Approved");
+//             console.log("Selected Course Science");
+//             break;
+
+//         case "Commerce"
+//             console.log("Admission Approved");
+//             console.log("Selected Course Commerce");
+//             break;
+
+//         case "Arts"
+//             console.log("Admission Approved");
+//             console.log("Selected Course Arts");
+//             break;
+
+//         default:
+//             console.log("Invalid Course");
+//     }
+// } else {
+//     console.log("Not Eligible");
+// }
